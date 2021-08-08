6177cc8addbd2b1c5e35ba0ee95273ef74ac4476
colmultimedia <info@colmultimedia.com>
2021-08-08 14:59:35 -0500

    ajustes desafio 3

--- DESAFIO3/index.js
+++ DESAFIO3/index.js
@@ -1,18 +1,16 @@
-let acumulado = 0
-const procesoFinal =  (acumulado) => {
-    console.log(`El total acumulado es ${acumulado}`)
-}
+const procesoFinal = (acumulado) => {
+    console.log(`El total acumulado es ${acumulado}`);
+  };
 
-const letters = async (text, callback, time=1000) => {
-    let arr = text.split(" ")
-    for (let i = 0; i<arr.length; i++){
-        await timmer (time)
-        console.log(arr[i])
+  const letters = async (text, time, callback) => {
+    time === undefined ? (time = 1000) : time
+    let arr = text.split(' ')
+    for (let i = 0; i < arr.length; i++) {
+      await timmer(time)
+      console.log(arr[i])
     }
-    callback(`Proceso concluido. Cantidad de palabras en total: ${arr.length}`)
-    acumulado = arr.length+acumulado
-    
-}
+    callback(arr.length)
+  };
 
 const timmer = (ms) => {
     return new Promise ((res, rej) => {
@@ -24,14 +22,15 @@ const timmer = (ms) => {
 
     const orderedWords = async (text1, text2, text3) => {
         try {
-            await letters (text1, (word) => {
-                console.log(word)
+            let acumulado = 0;
+            await letters (text1, 100, (word) => {
+                acumulado += word
             },)
-            await letters (text2, (word) => {
-                console.log(word)
+            await letters (text2, 100, (word) => {
+                acumulado += word
             },)
-            await letters (text3, (word) => {
-                console.log(word)
+            await letters (text3, 100, (word) => {
+                acumulado += word
             })
             procesoFinal(acumulado)
 
