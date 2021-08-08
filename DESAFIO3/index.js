81fd5054f21278f2e2979d6de9913035c2902e6e
colmultimedia <info@colmultimedia.com>
2021-08-08 12:08:08 -0500

    desafio3

---
+++ DESAFIO3/index.js
@@ -0,0 +1,2 @@
+let consigna = {descripcion: 'en el callback de la funcion letters devuelve solamente el arr.length, crea otra funcion que reciba un parametro y que se llame proceso final, la ejecutas en el ultimo then, luego en cada then creas una variable y vas sumando las palabras que te va devolviendo cada callback y al final le pasas esa variable a la nueva funcion que creaste.'}
+console.log(consigna)
