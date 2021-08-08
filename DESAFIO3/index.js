52994b1d863aa8848ae06ddd4c840da038d077be
colmultimedia <50298394+colmultimedia@users.noreply.github.com>
2021-08-08 12:47:34 -0500

    acumulador al final agregado

--- DESAFIO3/index.js
+++ DESAFIO3/index.js
@@ -1,11 +1,44 @@
-let consigna = {descripcion: 'en el callback de la funcion letters devuelve solamente el arr.length, crea otra funcion que reciba un parametro y que se llame proceso final, la ejecutas en el ultimo then, luego en cada then creas una variable y vas sumando las palabras que te va devolviendo cada callback y al final le pasas esa variable a la nueva funcion que creaste.'}
-console.log(consigna)
+let acumulado = 0
+const procesoFinal =  (acumulado) => {
+    console.log(`El total acumulado es ${acumulado}`)
+}
 
 const letters = async (text, callback, time=1000) => {
     let arr = text.split(" ")
     for (let i = 0; i<arr.length; i++){
         await timmer (time)
-        console.log(ar[i])
+        console.log(arr[i])
     }
-    callback()
+    callback(`Proceso concluido. Cantidad de palabras en total: ${arr.length}`)
+    acumulado = arr.length+acumulado
+    
 }
+
+const timmer = (ms) => {
+    return new Promise ((res, rej) => {
+        setTimeout(()=> {
+            res()
+        },ms)
+    })
+}
+
+    const orderedWords = async (text1, text2, text3) => {
+        try {
+            await letters (text1, (word) => {
+                console.log(word)
+            },)
+            await letters (text2, (word) => {
+                console.log(word)
+            },)
+            await letters (text3, (word) => {
+                console.log(word)
+            })
+            procesoFinal(acumulado)
+
+        }catch(err){
+        console.log(err)
+
+        }
+}   
+
+orderedWords('Mattheuv Osorio', 'Este es el desafio No 3', 'con el uso de split que no entiendo Silvio')
